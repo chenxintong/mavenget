62f10745e45310d5b26ac7c8c8c60b620223b15a
chenxintong <244239019@qq.com>
2018-04-20 17:55:05 +0800

    第一次提交

---
+++ src/main/java/MyJava.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ *
+ * @author: 24423
+ * Date: 2018-04-20
+ * Time: 17:52
+ */
+public class MyJava {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+    }
+}
